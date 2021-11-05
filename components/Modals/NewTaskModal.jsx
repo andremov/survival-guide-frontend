44cfd892a0f57f851e7c7f38b0ace9152426483f
Andres-XL <amovilla1103@gmail.com>
2021-11-05 11:18:00 -0500

    improve new task form

--- components/Modals/NewTaskModal.jsx
+++ components/Modals/NewTaskModal.jsx
@@ -11,62 +11,61 @@ export function NewTaskModal( { closeCallback } ) {
 	const [ taskData, setTaskData ] = React.useState( {
 		status : 'PENDING'
 	} )
-	const [formState, setFormState] = React.useState(0);
+	const [ formState, setFormState ] = React.useState( 0 );
 	const [ hasError, setError ] = React.useState( false );
 
 	const handleChange = ( name, value ) => {
 		setTaskData( { ...taskData, [ name ] : value } )
-		setError(false)
+		setError( false )
 	}
+
 	const createTaskRequest = () => {
-		setFormState(1)
+		setFormState( 1 )
 		createTask( taskData )
-			.then(() => {
+			.then( () => {
 				setFormState( 2 )
-				setTimeout(closeCallback, 3000)
-			})
+				setTimeout( closeCallback, 3000 )
+			} )
 			.catch( () => {
 				setFormState( 0 )
 				setError( true )
 			} )
 	}
 
+	if ( formState === 1 ) {
+		return <Modal>
+			<RequestingForm/>
+		</Modal>
+	}
+
+	if ( formState === 2 ) {
+		return <Modal>
+			<SuccessForm/>
+		</Modal>
+	}
+
 	return (
 		<Modal
 			title={ 'Nueva tarea' }
 		>
-			{
-				formState === 0 && <>
-					<Input value={ taskData.name } name={ 'name' } placeholder={ 'Nombre' } onChange={ handleChange }/>
-					<Input value={ taskData.information } name={ 'information' } placeholder={ 'Información' }
-					       onChange={ handleChange }/>
-					<Input value={ taskData.due_date } name={ 'due_date' } placeholder={ 'Fecha limite' } onChange={ handleChange }
-					       type={ 'date' }/>
-					<DualInput>
-						<Button
-							onClick={ closeCallback }
-							label={ 'Cancelar' }
-							color={ 'gray' }
-						/>
-						<Button
-							onClick={ createTaskRequest }
-							icon={ faPlus }
-							label={ 'Agregar' }
-							color={ 'green' }
-						/>
-					</DualInput>
-				</>
-			}
-			{
-				formState === 1 && <>
-					<RequestingForm/>
-				</>
-			}
-			{
-				formState === 2 && <>
-					<SuccessForm/>
-				</>
-			}
+			<Input value={ taskData.name } name={ 'name' } placeholder={ 'Nombre' } onChange={ handleChange }/>
+			<Input value={ taskData.information } name={ 'information' } placeholder={ 'Información' }
+			       onChange={ handleChange }/>
+			<Input value={ taskData.due_date } name={ 'due_date' } placeholder={ 'Fecha limite' } onChange={ handleChange }
+			       type={ 'date' }/>
+			<DualInput>
+				<Button
+					onClick={ closeCallback }
+					label={ 'Cancelar' }
+					color={ 'gray' }
+				/>
+				<Button
+					onClick={ createTaskRequest }
+					icon={ faPlus }
+					label={ 'Agregar' }
+					color={ 'green' }
+				/>
+			</DualInput>
 		</Modal>
 	);
 }
---
+++ components/Modals/TaskInfoModal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function TaskInfoModal() {
+	return (
+		<div>
+			Hello world!
+		</div>
+	);
+}
+
