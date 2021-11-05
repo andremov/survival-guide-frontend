2ae33b30cd9c5efd4dff9ce70a465e68991204bf
Andres-XL <amovilla1103@gmail.com>
2021-11-05 01:39:31 -0500

    use sass, create header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/main.scss'
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Header/>
+    <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
