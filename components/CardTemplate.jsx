33abf23b4c02b7fb4203e678b45e2253e3ac0824
Andres-XL <amovilla1103@gmail.com>
2021-11-06 01:03:01 -0500

    fix title on card

--- components/CardTemplate.jsx
+++ components/CardTemplate.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Button } from './Buttons';
 
-export function CardTemplate({title='', className='', buttons=[], children}) {
+export function CardTemplate( { title = '', className = '', buttons = [], children } ) {
 	return (
-		<div className={ `card ${className}` }>
-			<div className={ 'card-header' }>
-				<h2>
-					{ title }
-				</h2>
-				{ buttons.length > 0 && <div className={ 'header-buttons' }>
-					{
-						buttons.map( ( item, i ) => <Button
-							className={ item.className }
-							color={ item.color }
-							icon={ item.icon }
-							onClick={ item.onClick }
-							key={ i }
-						/> )
-					}
-				</div> }
-			</div>
-			<hr/>
-			<div className={'card-body'}>
+		<div className={ `card ${ className }` }>
+			{
+				title && <>
+					<div className={ 'card-header' }>
+						<h2>
+							{ title }
+						</h2>
+						{ buttons.length > 0 && <div className={ 'header-buttons' }>
+							{
+								buttons.map( ( item, i ) => <Button
+									className={ item.className }
+									color={ item.color }
+									icon={ item.icon }
+									onClick={ item.onClick }
+									key={ i }
+								/> )
+							}
+						</div> }
+					</div>
+					<hr/>
+				</>
+			}
+			<div className={ 'card-body' }>
 				{ children }
 			</div>
 		</div>
