f12f562932546a078863920090a113e1b95cc7ac
Andres-XL <amovilla1103@gmail.com>
2021-11-08 13:32:08 -0500

    fix array size client side error

--- components/LineChart.jsx
+++ components/LineChart.jsx
@@ -6,7 +6,7 @@ import { getMonthlies } from '../services/monthlySlice';
 const parseMonthlies = monthlies => {
 	const startMonth = Math.min(...monthlies.map(item => item.month_id) )
 	const endMonth = Math.max(...monthlies.map(item => item.month_id) )
-	let labels = [ ...new Array( (endMonth-startMonth)+1 ).keys() ].map(item => item+startMonth)
+	let labels = [ ...new Array( Math.max((endMonth-startMonth)+1,1) ).keys() ].map(item => item+startMonth)
 
 	let dueData = labels.map(label => monthlies.filter(monthly => monthly.month_id === label).map(monthly => monthly.amount_due).reduce((a,b) => a + b, 0))
 	let paidData = labels.map(label => monthlies.filter(monthly => monthly.month_id === label).map(monthly => monthly.amount_paid ?? 0).reduce((a,b) => a + b, 0))
