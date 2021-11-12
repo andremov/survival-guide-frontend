336167b249f92bfaaf64b5ce4ea661c490b65b85
Andres-XL <amovilla1103@gmail.com>
2021-11-11 22:54:47 -0500

    add monthly status filter breadcrumbs

--- components/FilterBreadcrumbs.jsx
+++ components/FilterBreadcrumbs.jsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function FilterBreadcrumbs() {
 	const dispatch = useDispatch()
-	const { person_name, institution } = useSelector( getFilters )
+	const { person_name, institution, monthly_status } = useSelector( getFilters )
 
 	const onToggleFilter = ( type, value ) => dispatch( setFilters( { type, value } ) )
 
@@ -14,8 +14,15 @@ export function FilterBreadcrumbs() {
 		<div className={ 'crumb-container' }>
 			{ person_name.map( ( item, i ) => <Crumb value={item} key={ i } removeFilter={() => onToggleFilter('person_name', item)}/> ) }
 			{ institution.map( ( item, i ) => <Crumb value={item} key={ i } removeFilter={() => onToggleFilter('institution', item)}/> ) }
+			{ monthly_status.map( ( item, i ) => <Crumb value={dictionary[item]} key={ i } removeFilter={() => onToggleFilter('monthly_status', item)}/> ) }
 		</div>
 	);
+}
+
+const dictionary = {
+	PAID: 'Pagado',
+	PENDING: 'Pendiente',
+	UNREPORTED: 'Sin Reportar'
 }
 
 const Crumb = ( { value, removeFilter } ) => <div className={ 'crumb' } onClick={removeFilter}>
